6ef1a932f6d39db0bece2941633388d64477e396
Alejandro Torres <jealtolo24@gmail.com>
2026-04-16 20:38:03 -0600

    cree la entidad Order
    cree la entidad Order

---
+++ Coffee.Core/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Coffee.Core.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int? CustomerId { get; set; }
+    public int PaymentMethodId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public decimal Total { get; set; }
+    public string Status { get; set; } = "Pending";
+}
