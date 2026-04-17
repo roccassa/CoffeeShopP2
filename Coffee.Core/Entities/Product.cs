a164cbffa3df7f15ced1eeb11471f67ec90ac623
Alejandro Torres <jealtolo24@gmail.com>
2026-04-16 20:21:27 -0600

    cree la entidad producto
    cree la entidad producto

---
+++ Coffee.Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee.Core.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public int CategoryId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+}
