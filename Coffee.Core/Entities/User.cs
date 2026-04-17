444809eed0667d1374c721ca090757eef27b5811
Alejandro Torres <jealtolo24@gmail.com>
2026-04-16 20:34:01 -0600

    cree entidad Usuario
    cree entidad Usuario

---
+++ Coffee.Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee.Core.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public int RoleId { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+}
