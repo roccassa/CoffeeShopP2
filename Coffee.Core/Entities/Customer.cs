47b95a8c2aecc2888f6425410b9caae843345603
Alejandro Torres <jealtolo24@gmail.com>
2026-04-16 20:44:27 -0600

    cree la entidad Customer

---
+++ Coffee.Core/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Coffee.Core.Entities;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public int LoyaltyPoints { get; set; }
+}
