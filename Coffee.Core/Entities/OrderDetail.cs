22fa2123bb8e7e5a3126e2cdcbdd343b9b5dba9c
Alejandro Torres <jealtolo24@gmail.com>
2026-04-16 20:42:32 -0600

    cree la entidad OrderDetail
    cree la entidad OrderDetail

---
+++ Coffee.Core/Entities/OrderDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee.Core.Entities;
+
+public class OrderDetail
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductVariantId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
