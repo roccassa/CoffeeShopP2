1b17a14e347538fa3dfc3abb72e2144923f92d8c
Alejandro Torres <jealtolo24@gmail.com>
2026-04-16 20:28:02 -0600

    cree entidad ProductVariant
    cree entidad ProductVariant

---
+++ Coffee.Core/Entities/ProductVariant.cs
@@ -0,0 +1,9 @@
+﻿namespace Coffee.Core.Entities;
+
+public class ProductVariant
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public string Size { get; set; } = string.Empty; 
+    public decimal Price { get; set; }
+}
